3f3ba6307e17c7b575cc47839eafb51f6cc8e9ce
TweakyTurnip875 <44481502+TweakyTurnip875@users.noreply.github.com>
2021-09-21 09:15:15 -0600

    Add files via upload

---
+++ src/com/noah/httpfetch/Main/Main.java
@@ -0,0 +1,52 @@
+package com.noah.httpfetch.Main;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.io.BufferedReader;
+import java.lang.StringBuilder;
+import java.io.InputStreamReader;
+import java.io.*;
+
+public class Main {
+
+	public static String getData() {
+		HttpURLConnection c = null;
+		try {
+			URL url = new URL("https://jsonplaceholder.typicode.com/posts/1/");
+			c = (HttpURLConnection) url.openConnection();
+			c.setRequestMethod("GET");
+			c.setUseCaches(false);
+			c.setAllowUserInteraction(false);
+			c.addRequestProperty("Content-length", "0");
+			c.connect();
+
+			int status = c.getResponseCode();
+
+			if(status == 200 || status == 201) {
+				BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+				StringBuilder sb = new StringBuilder();
+				String line;
+
+				while((line = br.readLine()) != null) {
+					sb.append(line + "\n");
+				}
+				br.close();
+				System.out.println();
+			}
+
+		} catch (Exception MalformedURLException) {
+			System.out.println("Error in Main.java file\n" + MalformedURLException);
+		} finally {
+			if(c != null) {
+				try {
+					c.disconnect();
+				} catch(Exception ex) {
+					System.out.println("error");
+				}
+			}
+		}
+		return null;
+	}
+
+}
